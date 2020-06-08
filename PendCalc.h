634685c0984a0e702fa2aa0014162dfc17bf2222
stellka <ssd2001.mozdok>
2020-06-08 13:18:39 +0300

    OpenGLPend
    OpenGLPend

---
+++ PendCalc.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+// Класс для расчета показателей маятника
+class PendCalc
+{
+private:
+	// Входящие значения
+	double A0 ;
+	double V0 ;
+	double Len ;
+	// Расчитанные амплитуды
+	double Am ;
+	double Vm ;
+	// Коэффициент маятника
+	double w ;
+	// Обновление показателей
+	void update() ;
+public:
+	// Установка значений
+	void setA0(double A0) ;
+	void setV0(double V0) ;
+	void setLen(double Len) ;
+	// Получение отклонения, скорости и энергий
+	double getA(double t) ;
+	double getV(double t) ;
+	double getE(double t) ;
+	double getK(double t) ;
+	// А это тесты, вызываемые в начале программы
+	static bool execTests() ;
+};
+
